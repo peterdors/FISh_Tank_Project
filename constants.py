e7bdf896768195fab0bbf8ad9083259447763a2e
peterdors <peterdorsaneo@gmail.com>
2019-07-03 00:30:06 -0400

    Cleanup of python scripts.

--- constants.py
+++ constants.py
@@ -1,3 +1,7 @@
-URL = 'https://careers.peopleclick.com/careerscp/client_FISGlobal/external/search/search.html'
+# Peter Dorsaneo
+# 
+# Summer 2019 FIS(h) Tank Project, Team 3
+# ========================================
+# This is where we will house all the constants for the main python script. 
 
 JOB_SEARCH_URL = 'https://careers.peopleclick.com/careerscp/client_FISGlobal/external/results/searchResult.html'
--- main.py
+++ main.py
@@ -1,77 +1,35 @@
+# Peter Dorsaneo
+# 
+# Summer 2019 FIS(h) Tank Project, Team 3
+# ========================================
+# Python script to load a Chrome web page of the FIS Careers website and auto
+# load the list of available intern positions.
+# 
+# USAGE: python main.py
+
 from constants import *
-import requests
-import urllib.request
-import time, contextlib
-from bs4 import BeautifulSoup
-from time import sleep
-
-def simple_get(url):
-    """
-    Attempts to get the content at `url` by making an HTTP GET request.
-    If the content-type of response is some kind of HTML/XML, return the
-    text content, otherwise return None.
-    """
-    try:
-        with contextlib.closing(requests.get(url, stream=True)) as resp:
-            if is_good_response(resp):
-                return resp.content
-            else:
-                return None
-
-    except RequestException as e:
-        log_error('Error during requests to {0} : {1}'.format(url, str(e)))
-        return None
-
-
-def is_good_response(resp):
-    """
-    Returns True if the response seems to be HTML, False otherwise.
-    """
-    content_type = resp.headers['Content-Type'].lower()
-    return (resp.status_code == 200 
-            and content_type is not None 
-            and content_type.find('html') > -1)
-
-
-def log_error(e):
-    """
-    It is always a good idea to log errors. 
-    This function just prints them, but you can
-    make it do anything.
-    """
-    print(e)
-
-
 from splinter import Browser
-
-from selenium import webdriver
-
 
 browser = Browser('chrome')
 
-# Visit URL
+# Visit URL.
 url = JOB_SEARCH_URL
 browser.visit(url)
 
+# Find and click on the 'Intern' positions from the website. 
 option = browser.find_by_xpath('//select[@id="com.peopleclick.cp.formdata.JPM_DURATION"]//option[@value="7"]')
-
 option.click()
 
+# Javascript injection to unselect the option for 'all' positions. 
+# Without this, the webpage will still load all the open positions from the site.  
 browser.execute_script('document.getElementsByTagName("select")[3].getElementsByTagName("option")[0].selected = false')
 
-sleep(2)
-
-# Find and click the 'search' button
+# Find and click the 'Search' button from the website. 
 button = browser.find_by_id('sp-searchButton')
-
-
-# Interact with elements
 button.click()
 
-if browser.is_text_present('Product Manager, Advisory'):
-    print("Yes, the official website was found!")
-else:
-    print("No, it wasn't found... We need to improve our SEO techniques")
+
+
 
 
 
