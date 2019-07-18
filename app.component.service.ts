4f9cf154cc28f2afcc400c9fef682f3a9bc34cf6
Nick <35236132+Solid-Sponge@users.noreply.github.com>
2019-07-18 08:12:22 -0500

    Initial Angular Front-End Files
    These are the main component, service, routing, etc, files for the Angular front end for our FIS(h) tank website.

---
+++ app.component.service.ts
@@ -0,0 +1,5 @@
+import { Inject } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Observable} from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { MainComponent, UserInfo } from './app.component';
---
+++ app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { MainComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [MainComponent]
+})
+export class AppModule { }
