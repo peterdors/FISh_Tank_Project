10baa5af154938da025f4dc1e3c15077223b096b
Peter Dorsaneo <34838907+peterdors@users.noreply.github.com>
2019-07-19 08:44:49 -0400

    Delete app.component.service.ts

--- app.component.service.ts
+++
@@ -1,5 +0,0 @@
-import { Inject } from '@angular/core';
-import { Injectable } from '@angular/core';
-import { Observable} from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { MainComponent, UserInfo } from './app.component';
