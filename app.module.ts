eba977648dc002cce5065f1bf83fa46de64bfe82
Nick <35236132+Solid-Sponge@users.noreply.github.com>
2019-07-23 13:36:18 -0500

    Added Angular materials/forms for styling
    Added Angular tags and matInput attribute for modern, responsive front-end UI. "<mat-form-field>" tag logs error in my browser console, but tested and works on a different machine.

--- app.module.ts
+++ app.module.ts
@@ -1,11 +1,18 @@
+//angular imports
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import {
+  MatSelectModule, MatSelect, MatSelectTrigger, MatOption,
+  MatDatepicker, MatDatepickerModule, MatNativeDateModule,
+  MatTabsModule, MatButtonModule, MatCardModule, MatCheckboxModule,
+  MatFormFieldModule, MatIconModule, MatInputModule, MatListModule, MatTable, MatTableModule, MatRadioButton, MatRadioGroup, MatRadioModule, MatPaginator, MatPaginatorModule, MatExpansionModule
+} from '@angular/material';
+import { DateAdapter, MAT_DATE_FORMATS, MatDateFormats, ThemePalette } from '@angular/material/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSelectModule, MatTabsModule, MatButtonModule, MatCardModule, MatCheckboxModule, 
-  MatFormFieldModule, MatInputModule } from '@angular/material';
-
 import { AppRoutingModule } from './app-routing.module';
 import { MainComponent } from './app.component';
 import { MainComponentService } from './app.component.service';
@@ -15,6 +22,21 @@ import { MainComponentService } from './app.component.service';
     MainComponent
   ],
   imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    CommonModule,
+    MatSelectModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  exports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
